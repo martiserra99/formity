0c49982d9551fb61d6c8fff8eb18758f47279c27
Marti Serra <martiserramolina99@gmail.com>
2024-07-16 11:35:34 +0200

    updated styles

--- example/index.tsx
+++ example/index.tsx
@@ -2,12 +2,19 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import { Theme } from '@radix-ui/themes';
+
 import Example from './src/example';
 
 import './global.css';
+import './theme-config.css';
 
 const App = () => {
-  return <Example />;
+  return (
+    <Theme appearance="dark" panelBackground="translucent">
+      <Example />
+    </Theme>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
