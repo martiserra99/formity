0179e26488b3ae6705136342b51dd3dab6fe1b26
Marti Serra <martiserramolina99@gmail.com>
2024-07-16 11:43:32 +0200

    updated example

--- example/src/example/index.tsx
+++ example/src/example/index.tsx
@@ -1,21 +1,37 @@
+// import * as React from 'react';
+
+// import { Formity } from '../../..';
+
+// import components from './components';
+// import schema from './schema';
+
+// import Form from './form';
+
+// export default function Example() {
+//   const handleSubmit = React.useCallback(() => {}, []);
+
+//   return (
+//     <Formity
+//       components={components}
+//       form={Form}
+//       schema={schema}
+//       onSubmit={handleSubmit}
+//     />
+//   );
+// }
+
 import * as React from 'react';
 
-import { Formity } from '../../..';
-
-import components from './components';
-import schema from './schema';
-
-import Form from './form';
+import Center from '../components/center';
+import Card from '../components/card';
+import Result from '../components/result';
 
 export default function Example() {
-  const handleSubmit = React.useCallback(() => {}, []);
-
   return (
-    <Formity
-      components={components}
-      form={Form}
-      schema={schema}
-      onSubmit={handleSubmit}
-    />
+    <Center>
+      <Card>
+        <Result result={[{ a: '1' }]} />
+      </Card>
+    </Center>
   );
 }
