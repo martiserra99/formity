e4bc7fd9989f93da948f1af93e4de2873d963afb
Marti Serra <martiserramolina99@gmail.com>
2025-01-02 18:29:17 +0100

    Added comments in utils.ts

--- packages/system/src/types/utils.ts
+++ packages/system/src/types/utils.ts
@@ -1,3 +1,9 @@
+/**
+ * These utility types are designed to simplify and enhance the process of defining types
+ * that extend `ListValues`. By using these types, you can create more readable and concise
+ * code when structuring a multi-step form.
+ */
+
 import { ListValues } from "./values";
 
 export type Cond<Values extends { then: ListValues; else: ListValues }> = {
