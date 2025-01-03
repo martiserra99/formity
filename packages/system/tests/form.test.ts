2a5ea9bff1fed0e9269bf68744cfcaca94eb25d1
Marti Serra <martiserramolina99@gmail.com>
2025-01-03 19:37:05 +0100

    Added tests for the getForm function

---
+++ packages/system/tests/form.test.ts
@@ -0,0 +1,110 @@
+import { describe, expect, it } from "vitest";
+
+import {
+  Schema,
+  Variables,
+  Form,
+  Cond,
+  Loop,
+  Flow,
+  getForm,
+} from "../src/index";
+
+describe("getForm", () => {
+  it("renders the form from the schema that is in the current position", () => {
+    type Values = [
+      Variables<object>,
+      Cond<{ then: [Loop<[Form<object>]>]; else: [] }>
+    ];
+    const schema: Schema<object, Values, object, object> = [
+      { variables: () => ({}) },
+      {
+        cond: {
+          if: () => true,
+          then: [
+            {
+              loop: {
+                while: () => true,
+                do: [
+                  {
+                    form: {
+                      values: () => ({}),
+                      render: () => ({
+                        hello: "world",
+                      }),
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+          else: [],
+        },
+      },
+    ];
+    const flow: Flow = {
+      cursors: [
+        {
+          path: [
+            { type: "list", slot: 1 },
+            { type: "cond", path: "then", slot: 0 },
+            { type: "loop", slot: 0 },
+          ],
+          values: {},
+        },
+      ],
+      entries: { type: "list", list: {} },
+    };
+    const form = getForm<object, Values, object, object>(
+      flow,
+      schema,
+      {},
+      () => {},
+      () => {},
+      () => flow,
+      () => {}
+    );
+    expect(form).toEqual({ hello: "world" });
+  });
+
+  it("uses the values from the form defined in the schema", () => {
+    type Values = [Form<{ name: string }>];
+    const schema: Schema<object, Values, object, object> = [
+      {
+        form: {
+          values: () => ({
+            name: ["John", []],
+          }),
+          render: ({ values }) => ({
+            defaultValues: {
+              name: values.name,
+            },
+          }),
+        },
+      },
+    ];
+    const flow: Flow = {
+      cursors: [
+        {
+          path: [{ type: "list", slot: 0 }],
+          values: {},
+        },
+      ],
+      entries: { type: "list", list: {} },
+    };
+    const form = getForm<object, Values, object, object>(
+      flow,
+      schema,
+      {},
+      () => {},
+      () => {},
+      () => flow,
+      () => {}
+    );
+    expect(form).toEqual({
+      defaultValues: {
+        name: "John",
+      },
+    });
+  });
+});
