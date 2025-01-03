88150769edf14adc9861ff1757f33d0b6d6004e4
Marti Serra <martiserramolina99@gmail.com>
2025-01-03 14:07:25 +0100

    Added comments in values.ts

--- packages/system/src/types/values.ts
+++ packages/system/src/types/values.ts
@@ -1,13 +1,6 @@
 /**
- * The `ListSchema` type accepts a generic parameter that must extend `ListValues`.
- *
- * `ListValues` is an array containing `ItemValues`, which is a union of specific types:
- * `FlowValues`, `FormValues`, `YieldValues`, `ReturnValues`, and `VariablesValues`.
- *
- * By extending `ListValues`, we can define the structure of a multi-step form,
- * with the values used in each step.
+ * This type is meant to be extended and is used to define the structure and values of any element in a multi-step form.
  */
-
 export type ItemValues =
   | FlowValues
   | FormValues
@@ -15,10 +8,19 @@ export type ItemValues =
   | ReturnValues
   | VariablesValues;
 
+/**
+ * This type is meant to be extended and is used to define the structure and values of any flow element in a multi-step form.
+ */
 export type FlowValues = ListValues | CondValues | LoopValues;
 
+/**
+ * This type is meant to be extended and is used to define the structure and values of a list element in a multi-step form.
+ */
 export type ListValues = ItemValues[];
 
+/**
+ * This type is meant to be extended and is used to define the structure and values of a condition element in a multi-step form.
+ */
 export type CondValues = {
   type: "cond";
   cond: {
@@ -27,6 +29,9 @@ export type CondValues = {
   };
 };
 
+/**
+ * This type is meant to be extended and is used to define the structure and values of a loop element in a multi-step form.
+ */
 export type LoopValues = {
   type: "loop";
   loop: {
@@ -34,21 +39,33 @@ export type LoopValues = {
   };
 };
 
+/**
+ * This type is meant to be extended and is used to define the values of a form element in a multi-step form.
+ */
 export type FormValues = {
   type: "form";
   form: object;
 };
 
+/**
+ * This type is meant to be extended and is used to define the values of a yield element in a multi-step form.
+ */
 export type YieldValues = {
   type: "yield";
   yield: object;
 };
 
+/**
+ * This type is meant to be extended and is used to define the values of a return element in a multi-step form.
+ */
 export type ReturnValues = {
   type: "return";
   return: object;
 };
 
+/**
+ * This type is meant to be extended and is used to define the values of a variables element in a multi-step form.
+ */
 export type VariablesValues = {
   type: "variables";
   variables: object;
