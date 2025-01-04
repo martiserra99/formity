66eebacfd7377ad3979f6fdf9cec131e5da0f709
Marti Serra <martiserramolina99@gmail.com>
2025-01-04 12:03:21 +0100

    Added tests for schema flow list

---
+++ packages/system/src/utils/schema/flow.list.test.ts
@@ -0,0 +1,65 @@
+import { describe, expect, it } from "vitest";
+
+import type { ListSchema } from "../../types/schema/static";
+import type { Position } from "src/types/flow/position";
+
+import { into, next } from "./flow.list";
+
+describe("ListSchema", () => {
+  describe("into", () => {
+    it("navigates into the `ListSchema` object", () => {
+      const schema: ListSchema = [
+        {
+          form: {
+            values: () => ({}),
+            render: () => ({}),
+          },
+        },
+      ];
+      const position = into(schema);
+      expect(position).toEqual({ type: "list", slot: 0 });
+    });
+
+    it("doesn't navigate into the `ListSchema` object", () => {
+      const schema: ListSchema = [];
+      const position = into(schema);
+      expect(position).toEqual(null);
+    });
+  });
+
+  describe("next", () => {
+    it("navigates to the next item in the `ListSchema` object", () => {
+      const schema: ListSchema = [
+        {
+          form: {
+            values: () => ({}),
+            render: () => ({}),
+          },
+        },
+        {
+          form: {
+            values: () => ({}),
+            render: () => ({}),
+          },
+        },
+      ];
+      const current: Position = { type: "list", slot: 0 };
+      const position = next(schema, current);
+      expect(position).toEqual({ type: "list", slot: 1 });
+    });
+
+    it("doesn't navigate to the next item in the `ListSchema` object", () => {
+      const schema: ListSchema = [
+        {
+          form: {
+            values: () => ({}),
+            render: () => ({}),
+          },
+        },
+      ];
+      const current: Position = { type: "list", slot: 0 };
+      const position = next(schema, current);
+      expect(position).toEqual(null);
+    });
+  });
+});
