225ddc96369c6de43de567206dede7dffabded3f
Marti Serra <martiserramolina99@gmail.com>
2025-01-16 13:10:42 +0100

    Built project

--- apps/react/cypress.config.js
+++ apps/react/cypress.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  video: false,
+  screenshotOnRunFailure: false,
   component: {
     devServer: {
       framework: "react",
