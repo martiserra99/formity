8be437234eebefeaea697c4459e39e5dcc57507a
Marti Serra <martiserramolina99@gmail.com>
2025-02-11 16:46:46 +0100

    Added missing data-cy attribute in button

--- apps/react/src/components/user-interface/button.tsx
+++ apps/react/src/components/user-interface/button.tsx
@@ -8,6 +8,7 @@ export default function Button({
 }: ComponentPropsWithoutRef<"button">) {
   return (
     <button
+      data-cy="button"
       className={cn(
         "block w-full rounded-full bg-indigo-500 px-6 py-4 text-base text-white hover:bg-indigo-400",
         "focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-offset-2 focus:ring-offset-black",
